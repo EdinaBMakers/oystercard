28181f279227bf0de4b7707f80724578f1c56ce2
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-05-08 20:55:53 +0100

    Track entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,11 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :entry_station
   DEFAULT_MAX_BALANCE = 90
   SINGLE_FARE = 1
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(amount)
@@ -16,17 +16,17 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    @entry_station != nil
   end
 
-  def touch_in
+  def touch_in(station)
     raise "Balance is not enough for a single journey" if under_minimum_balance?
-    @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
     deduct(SINGLE_FARE)
-    @in_journey = false
+    @entry_station = nil
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,7 @@ require 'oystercard'
 
 describe Oystercard do
   let(:subject) { Oystercard.new }
+  let(:station) { double :station }
 
   it 'has 0 balance by default' do
     expect(subject.balance).to eq(0)
@@ -26,28 +27,28 @@ describe Oystercard do
 
   it 'is in use after touch in' do
     subject.top_up(Oystercard::SINGLE_FARE)
-    subject.touch_in
+    subject.touch_in(station)
 
     expect(subject).to be_in_journey
   end
 
   it 'is not in use after touch out' do
     subject.top_up(Oystercard::SINGLE_FARE)
-    subject.touch_in
+    subject.touch_in(station)
     subject.touch_out
 
     expect(subject).not_to be_in_journey
   end
 
   it 'raises error when touching in with balance lower than a single fare' do
-    expect { subject.touch_in }.to \
+    expect { subject.touch_in(station) }.to \
     raise_error("Balance is not enough for a single journey")
   end
 
   it 'charges the card on touch out' do
     subject.top_up(3 * Oystercard::SINGLE_FARE)
-    subject.touch_in
-    
+    subject.touch_in(station)
+
     expect { subject.touch_out }.to change \
     {subject.balance}.by(-1 * Oystercard::SINGLE_FARE)
   end
@@ -55,6 +56,25 @@ describe Oystercard do
   it 'does not charge the card on touch in' do
     subject.top_up(3 * Oystercard::SINGLE_FARE)
 
-    expect { subject.touch_in }.not_to change { subject.balance }
+    expect { subject.touch_in(station) }.not_to change { subject.balance }
+  end
+
+  it 'does not have entry station by default' do
+    expect(subject.entry_station).to be_nil
+  end
+
+  it 'remembers the entry station after the touch in' do
+    subject.top_up(Oystercard::SINGLE_FARE)
+    subject.touch_in(station)
+
+    expect(subject.entry_station).to eq(station)
+  end
+
+  it 'forgets the entry station on touch out' do
+    subject.top_up(Oystercard::SINGLE_FARE)
+    subject.touch_in(station)
+    subject.touch_out
+
+    expect(subject.entry_station).to be_nil
   end
 end
